0b56c065dd70aa89024e6621a7ce52dc3038afdc
Gareth Spanglett <gspanglett@gmail.com>
2022-01-05 21:58:59 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,21 @@
+# Copyright 2021, Sarah Cruthers and Gareth Spanglett
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
 #
